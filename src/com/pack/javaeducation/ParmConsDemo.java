334bfe7980fbf381b69e61bb970bc7d4a982b674
juntomoon <slavapozitif@yandex.ru>
2022-04-13 23:37:53 +0300

    #5

---
+++ src/com/pack/javaeducation/ParmConsDemo.java
@@ -0,0 +1,18 @@
+package com.pack.javaeducation;
+
+class Myclass5{
+    int x;
+
+    Myclass5(int i){
+        x = i;
+    }
+}
+
+class ParmConsDemo {
+    public static void main(String[] args) {
+        Myclass5 t1 = new Myclass5(10);
+        Myclass5 t2 = new Myclass5(88);
+
+        System.out.println(t1.x + " " + t2.x);
+    }
+}
