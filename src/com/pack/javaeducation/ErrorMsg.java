e8d56f7c434b3419b1d2dc0ee2c4aa70d0d6407e
juntomoon <slavapozitif@yandex.ru>
2022-05-02 03:19:49 +0300

    #Chapter-7-2-end

--- src/com/pack/javaeducation/ErrorMsg.java
+++ src/com/pack/javaeducation/ErrorMsg.java
@@ -25,6 +25,7 @@ class ErrorMsg {
 class FinalID{
     public static void main(String[] args) {
         ErrorMsg err = new ErrorMsg();
+
         System.out.println(err.getErrorMsg(err.OUTERR));
         System.out.println(err.getErrorMsg(err.DISKERR));
     }
