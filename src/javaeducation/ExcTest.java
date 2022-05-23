665ad74e3a811daf5f66ec674a1939e793e0bcdc
Mr.Neko <slavapozitif@yandex.ru>
2022-05-24 02:53:49 +0300

    Chapter-9-1

---
+++ src/javaeducation/ExcTest.java
@@ -0,0 +1,28 @@
+package javaeducation;
+/*
+    Исключение может быть сгенерировано одним методом,
+    а перехвачено другим
+ */
+class ExcTest {
+    // Генерация исключения
+    static void genException() {
+        int[] nums = new int[4];
+
+        System.out.println("До генерации исключения");
+        // Выход за пределы массива
+        nums[7] = 10;
+        System.out.println("Эта строка не будет отображаться");
+    }
+}
+class ExcDemo2 {
+    public static void main(String[] args) {
+
+        try {
+            ExcTest.genException();
+        }
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Выход за пределы массива!");
+        }
+        System.out.println("После инструкции catch");
+    }
+}
