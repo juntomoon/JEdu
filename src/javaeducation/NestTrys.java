3d7d152ed7779e3b4cc5032a76f0300384012920
Mr.Neko <slavapoziitf@yandex.ru>
2022-05-24 01:59:16 +0300

    Chapter-9-1

---
+++ src/javaeducation/NestTrys.java
@@ -0,0 +1,26 @@
+package javaeducation;
+// Использование вложенных блоков try
+class NestTrys {
+    public static void main(String[] args) {
+        // Длина массива numer превышает длину массива denom
+        int[] numer = {4, 8, 16, 32, 64, 128, 256, 512};
+        int[] denom = {2, 0, 4, 4, 0, 8};
+
+        try { // внешний блок try
+            for (int i = 0; i < numer.length; i++) {
+                try { // внутренний блок try
+                    System.out.println(numer[i] + " / " +
+                            denom[i] + " равно " +
+                            numer[i] / denom[i]);
+                } catch (ArithmeticException exc) {
+                    // Перехват исключения
+                    System.out.println("Попытка деления на нуль");
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            // Перехват исключения
+            System.out.println("Соответствующий элемент не найден");
+            System.out.println("Фатальная ошибка - выполнение программы прервано!");
+        }
+    }
+}
