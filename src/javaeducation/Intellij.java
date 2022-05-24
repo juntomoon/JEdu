9c94b6ee4a3431e13c41d36326e19fe06958b19f
Mr.Neko <slavapozitif@yandex.ru>
2022-05-24 15:53:49 +0300

    Chapter-9-1

---
+++ src/javaeducation/Intellij.java
@@ -0,0 +1,13 @@
+package javaeducation;
+/*
+TEst
+ */
+class Intellij {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        int[]= int test[5];
+        for (int i=0; i< test.length; i++){
+            System.out.println("Test");
+        }
+    }
+}
