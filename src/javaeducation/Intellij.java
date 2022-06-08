f2687f5a7d60f9df47623cac397171f77a2b2348
Mr.Neko <slavapozitif@yandex.ru>
2022-06-08 17:14:48 +0300

    Chapter-9-1

--- src/javaeducation/Intellij.java
+++ src/javaeducation/Intellij.java
@@ -4,10 +4,10 @@ TEst
  */
 class Intellij {
     public static void main(String[] args) {
-        System.out.println("Test");
-        int[]= int test[5];
-        for (int i=0; i< test.length; i++){
-            System.out.println("Test");
+        System.out.println("Out");
+        int[] test = new int[5];
+        for (int i=0; i<test.length; i++){
+            System.out.println("In");
         }
     }
 }
